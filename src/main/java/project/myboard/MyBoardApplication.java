7d799b2a9dbd797e8bb20563ea504f36d79cc040
hwnsng <amc214677@gmail.com>
2024-10-22 23:02:50 +0900

    .yml modify

--- src/main/java/project/myboard/MyBoardApplication.java
+++ src/main/java/project/myboard/MyBoardApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyBoardApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MyBoardApplication.class, args);
     }
